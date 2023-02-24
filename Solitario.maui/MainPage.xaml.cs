1bc8300978ef94b7d7faa4a302f7cdab52a82814
Giulio Sorrentino <gsorre84@gmail.com>
2023-02-24 22:26:26 +0100

    ora i toast sono multipiattaforma col maui toolkit.

--- Solitario.maui/MainPage.xaml.cs
+++ Solitario.maui/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Controls;
+﻿using CommunityToolkit.Maui.Alerts;
+using Microsoft.Maui.Controls;
 using org.altervista.numerone.framework;
 using System.Diagnostics;
 using System.Runtime.Intrinsics.X86;
@@ -10,10 +11,12 @@ public partial class MainPage : ContentPage
     int i, j, k;
     private UInt16[] vettore;
     private ulong mosse = 0;
+    private CancellationTokenSource cancellationTokenSource;
     public MainPage()
     {
         int a = 0, b = 0;
         InitializeComponent();
+        cancellationTokenSource = new CancellationTokenSource();
         vettore = new UInt16[30];
         ElaboratoreCarteSolitario e = new ElaboratoreCarteSolitario();
         Mazzo m = new Mazzo(e);
@@ -44,11 +47,9 @@ public partial class MainPage : ContentPage
     {
         UInt16 inizio, fine;
         int a = 0, b = 0;
-        Risultato.Text= "";
-        Risultato.TextColor = Colors.White;
-        if (Inizio.Text == null || Inizio.Text == "") { Risultato.Text = "La riga di inizio è vuota"; Risultato.TextColor = Colors.Red; return; }
+        if (Inizio.Text == null || Inizio.Text == "") { Snackbar.Make("La riga di inizio è vuota").Show(cancellationTokenSource.Token); return; }
 
-        if (Fine.Text == null || Fine.Text == "") { Risultato.Text = "La riga di fine è vuota"; Risultato.TextColor = Colors.Red; return; }
+        if (Fine.Text == null || Fine.Text == "") { Snackbar.Make("La riga di fine è vuota").Show(cancellationTokenSource.Token); return; }
 
         try
         {
@@ -56,7 +57,7 @@ public partial class MainPage : ContentPage
         }
         catch (FormatException ex)
         {
-            Risultato.Text = "La riga di inizio non è intera"; Risultato.TextColor = Colors.Red; return;
+            Snackbar.Make("La riga di inizio non è intera").Show(cancellationTokenSource.Token); return;
         }
         inizio--;
         try
@@ -65,12 +66,12 @@ public partial class MainPage : ContentPage
         }
         catch (FormatException ex)
         {
-            Risultato.Text = "La riga di fine non è intera"; Risultato.TextColor = Colors.Red; return;
+            Snackbar.Make("La riga di fine non è intera").Show(cancellationTokenSource.Token); return;
         }
         fine--;
-        if (inizio > 2) { Risultato.Text = "La riga di inizio non è nel range stabilito"; Risultato.TextColor = Colors.Red; return; }
-        if (fine > 2) { Risultato.Text = "La riga di fine non è nel range stabilito"; Risultato.TextColor = Colors.Red; return; }
-        if (inizio == fine) { Risultato.Text = "Le righe coincidono"; Risultato.TextColor = Colors.Red; return; }
+        if (inizio > 2) { Snackbar.Make("La riga di inizio non è nel range stabilito").Show(cancellationTokenSource.Token); return; }
+        if (fine > 2) { Snackbar.Make("La riga di fine non è nel range stabilito").Show(cancellationTokenSource.Token); return; }
+        if (inizio == fine) { Snackbar.Make("Le righe coincidono").Show(cancellationTokenSource.Token); return; }
         switch (inizio)
         {
             case 0: a = i; break;
@@ -86,10 +87,10 @@ public partial class MainPage : ContentPage
         Image c;
         if (a==-1)
         {
-            Risultato.Text = "La riga selezionata è vuota"; Risultato.TextColor = Colors.Red; return;
+            Snackbar.Make("La riga selezionata è vuota").Show(cancellationTokenSource.Token); return;
         }
         c = (Image)this.FindByName("carta" + vettore[inizio * 10 + a]);
-        if (b!=-1 && vettore[inizio * 10 + a] > vettore[fine*10+b]) { Risultato.Text = $"Operazione non consentita"; Risultato.TextColor = Colors.Red; return; }
+        if (b!=-1 && vettore[inizio * 10 + a] > vettore[fine*10+b]) { Snackbar.Make("Operazione non consentita").Show(cancellationTokenSource.Token); return; }
         vettore[fine * 10 + b+1] = vettore[inizio * 10 + a];
         vettore[inizio * 10 + a] = 0;
         Applicazione.SetRow(c, fine);
