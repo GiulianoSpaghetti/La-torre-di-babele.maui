4e153f954b65fb06f61f116e99828c9710b73197
Giulio Sorrentino <gsorre84@gmail.com>
2023-02-28 01:01:47 +0100

    ora mostra il device se si oniide il risultato su twitter.

--- Solitario.maui/App.xaml.cs
+++ Solitario.maui/App.xaml.cs
@@ -3,25 +3,18 @@
 public partial class App : Application
 {
     public static string piattaforma;
-
     public App()
 	{
 		InitializeComponent();
 
-        if (DeviceInfo.Current.Platform == DevicePlatform.Android)
-        {
-            MainPage = new AppShell();
-            piattaforma = "Android";
-        }
-        else if (DeviceInfo.Current.Platform == DevicePlatform.WinUI)
-        {
-            MainPage = new AppShellWindows();
-            piattaforma = "Windows NT";
-        }
-        else
-        {
-            MainPage = new AppShell();
-            piattaforma = "Unknown";
-        }
+#if WINDOWS
+        MainPage = new AppShellWindows();
+#else
+        MainPage = new AppShell();
+#endif
+        piattaforma = DeviceInfo.Current.Model;
+        if (piattaforma == "System Product Name")
+            piattaforma = "PC";
+
     }
 }
