4fda32d9b0bd0870bcf9556566080d76493db13e
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2023-03-06 00:42:32 +0100

    prove di compilazione con visual studio

--- Solitario.maui/MauiProgram.cs
+++ Solitario.maui/MauiProgram.cs
@@ -1,5 +1,4 @@
-﻿using CommunityToolkit.Maui;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
 namespace Solitario.maui;
 
@@ -9,8 +8,8 @@ public static class MauiProgram
 	{
 		var builder = MauiApp.CreateBuilder();
 		builder
-			.UseMauiApp<App>().UseMauiCommunityToolkit()
-            .ConfigureFonts(fonts =>
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
